fc26975250b00286f896606e13307a0b07b2e1a4
Abhisek Patel <abhisheksir6451@gmail.com>
2025-11-19 09:49:49 +0545

    Initial sharing of project

---
+++ workshop.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class workshop here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class workshop
+{ 
+  int age;//instance variable 
+  static int qty; //static variable 
+  public static void main(String [] args){
+    
+      //<className> <variable>New <className>();
+       
+      int age=19;// local variable
+      workshop v1=new workshop();
+      System.out.println(v1.age);
+      
+      System.out.println(workshop.qty);
+      
+    
+    
+    
+    }
+    
+    
+}
